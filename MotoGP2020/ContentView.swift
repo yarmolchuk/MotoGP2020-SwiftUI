e340defffeb62ea43ea3af9d8d992eec4e350ed2
yarmolchuk <yarmolchuk@gmail.com>
2019-11-22 22:47:10 +0200

    Initial Commit

---
+++ MotoGP2020/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MotoGP2020
+//
+//  Created by Dima Yarmolchuk on 22.11.2019.
+//  Copyright © 2019 Dima Yarmolchuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
